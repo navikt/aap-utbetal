08fca63f7b554179cab7f638e74b2e1d46e439d1
Frode Lindås <frode.lindas@nav.no>
2025-08-05 15:09:53 +0200

    Oppdatert behandlingsflyt og tilgang kontrakter.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,8 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val ktorVersion = "3.2.2"
 val komponenterVersjon = "1.0.302"
-val behandlingsflytVersjon= "0.0.384"
-val tilgangVersjon = "1.0.97"
+val behandlingsflytVersjon= "0.0.396"
+val tilgangVersjon = "1.0.99"
 
 plugins {
     id("utbetal.conventions")
