c4b46c378bfca2167318cb1997324888fb09a01d
Frode Lindås <frode.lindas@nav.no>
2025-09-01 12:02:09 +0200

    Fjernet ubrukt import

--- app/src/main/kotlin/no/nav/aap/utbetal/tilkjentytelse/TilkjentYtelseAPI.kt
+++ app/src/main/kotlin/no/nav/aap/utbetal/tilkjentytelse/TilkjentYtelseAPI.kt
@@ -12,7 +12,6 @@ import no.nav.aap.utbetal.httpCallCounter
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import javax.sql.DataSource
-import kotlin.time.measureTime
 
 private val log: Logger = LoggerFactory.getLogger("POST /tilkjentytelse")
 
