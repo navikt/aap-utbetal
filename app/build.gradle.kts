da9a2ae567900f9bd4a24a5b156d93844bc20cd4
Fredrik Meyer <fredrik.meyer@nav.no>
2025-09-12 09:08:45 +0200

    Variabel for versjoner slik at de oppdateres samtidig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ val ktorVersion = "3.2.3"
 val komponenterVersjon = "1.0.340"
 val behandlingsflytVersjon= "0.0.408"
 val tilgangVersjon = "1.0.115"
+val jacksonVersion = "2.19.2"
+val jupiterVersjon = "5.13.4"
 
 plugins {
     id("utbetal.conventions")
@@ -40,9 +42,9 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
 
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
 
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.19.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
     implementation("io.micrometer:micrometer-registry-prometheus:1.15.3")
     implementation("ch.qos.logback:logback-classic:1.5.18")
     implementation("net.logstash.logback:logstash-logback-encoder:8.1")
@@ -55,8 +57,8 @@ dependencies {
 
     testImplementation("no.nav.aap.kelvin:motor-test-utils:${komponenterVersjon}")
     testImplementation("com.nimbusds:nimbus-jose-jwt:10.4.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterVersjon")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jupiterVersjon")
     testImplementation("org.assertj:assertj-core:3.27.4")
     testImplementation("org.testcontainers:postgresql:1.21.3")
     constraints {
