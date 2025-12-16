6797459977e92409b6abcae1e2130254095e799b
Frode Lindås <frode.lindas@nav.no>
2025-12-16 12:48:15 +0100

    Sett opp datastream. Forsøk nr 2.

---
+++ dbflyway/src/main/resources/flyway/V1.21__del_2_datastream.sql
@@ -0,0 +1,8 @@
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
