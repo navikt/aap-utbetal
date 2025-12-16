7b35fbe8cef38a7f7e6f8eb94648603eb638fb34
Frode Lindås <frode.lindas@nav.no>
2025-12-16 12:22:47 +0100

    Sett opp datastream.

---
+++ dbflyway/src/main/resources/flyway/V1.18__datastream_oppsett.sql
@@ -0,0 +1,13 @@
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            ALTER USER "aap-utbetal" WITH REPLICATION;
+            CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
+
+            ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
+            GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "datastream";
+            ALTER USER "datastream" WITH REPLICATION;
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
