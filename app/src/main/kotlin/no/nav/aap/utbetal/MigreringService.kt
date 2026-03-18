fea40835b5ae796f431afaceb6c6e863127fea5a
Frode Lindås <frode.lindas@nav.no>
2026-03-18 10:34:43 +0100

    Whitelist for fnr som skal brukes til testing mot nytt utbetalingsgrensesnitt. Skal ikke merges før hel-ved er klar med sine endringer.

--- app/src/main/kotlin/no/nav/aap/utbetal/MigreringService.kt
+++ app/src/main/kotlin/no/nav/aap/utbetal/MigreringService.kt
@@ -15,8 +15,19 @@ class MigreringService {
         return false
     }
 
-    private val whitelisteMigrerteFødselsnummer = setOf<String>(
-        //TODO: Hent liste fra dolly
-    )
+    companion object {
+        private val whitelisteMigrerteFødselsnummer = setOf(
+            "29509000997",
+            "01509033583",
+            "30519028723",
+            "15419137747",
+            "13429149309",
+            "04509024311",
+            "31529003245",
+            "06509025930",
+            "14529037059",
+            "26529003764",
+        )
+    }
 
 }
