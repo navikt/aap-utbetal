197cc52f78a69b1c50490a3ccedf9c0bbdb80628
Frode Lindås <frode.lindas@nav.no>
2026-09-24 09:03:48 +0200

    AAP-2422: Legg inn igjen endring som gjør at vi sender dagsats + barnetillegg til utbetaling(brukes kun til utregning av riktig skatt). Bare slått på for andre miljøer enn prod i første omgang og kun fra en gitt dato. (#650)

--- app/src/main/kotlin/no/nav/aap/utbetal/felles/YtelseDetaljer.kt
+++ app/src/main/kotlin/no/nav/aap/utbetal/felles/YtelseDetaljer.kt
@@ -7,6 +7,8 @@ import no.nav.aap.komponenter.verdityper.GUnit
 import no.nav.aap.komponenter.verdityper.Prosent
 import java.time.LocalDate
 
+private val DATO_FOR_BARNETILLEGG_I_DAGSATS = LocalDate.parse("2026-09-28")
+
 data class YtelseDetaljer(
     val redusertDagsats: Beløp,
     val gradering: Prosent,
@@ -23,14 +25,14 @@ data class YtelseDetaljer(
 ) {
 
     /**
-     * Dagsats som skal brukes for å finne rekke skattekort. Skal brukes i feltet fastsattDagsats u utbetalingsperioden.
+     * Dagsats som skal brukes for å finne rekke skattekort. Skal brukes i feltet fastsattDagsats i utbetalingsperioden.
      */
-    fun dagsatsMedBarnetillegg(): Beløp {
-//        if (Miljø.erProd()) {
-            //Behold dagens logikk til vi har sjekket at det virker i testmiljø.
+    fun dagsatsMedBarnetillegg(periode: Periode): Beløp {
+        if (periode.fom.isBefore(DATO_FOR_BARNETILLEGG_I_DAGSATS) || Miljø.erProd()) {
+            // Behold gammel beregning av dagsats for gamle perioder.
             return dagsats
-//        }
-//        return dagsats.pluss(barnetillegg)
+        }
+        return dagsats.pluss(barnetillegg)
     }
 
 }
